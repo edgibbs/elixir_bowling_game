e99e6e32d55592a58615c3e8c7d995b060c75141
Ed Gibbs <edward_gibbs@yahoo.com>
2016-02-20 15:43:11 -0800

    Introduce simple frames

--- lib/bowling_game.ex
+++ lib/bowling_game.ex
@@ -1,5 +1,18 @@
 defmodule BowlingGame do
   def score(rolls) do
-    Enum.sum(rolls)
+    frames = Enum.chunk(rolls, 2)
+    sum(frames)
+  end
+
+  def sum(frames) do
+    score_frames(frames)
+  end
+
+  defp score_frames([frame|rest_of_frames]) do
+    Enum.sum(frame) + score_frames(rest_of_frames)
+  end
+
+  defp score_frames([]) do
+    0
   end
 end
